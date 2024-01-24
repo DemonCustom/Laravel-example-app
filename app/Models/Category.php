5aa16cae521bafb63a2b6ce8970b278fc9f41705
DemonCustom <bondarenko21117@mail.ru>
2024-01-24 21:46:23 +0300

    create Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int    $id
+ * @property string $name
+ * @property string $poster
+ * @property string $created_at
+ * @property string $updated_at
+ */
+
 class Category extends Model
 {
     use HasFactory;
