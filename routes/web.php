330e80db44806167cc85803c8ce150b26cd48136
DemonCustom <bondarenko21117@mail.ru>
2024-04-01 21:02:21 +0300

    Установка MoomShine

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Postcontroller::class , 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -26,6 +27,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('comments', CommentController::class)->only(['store', 'destroy']);
 });
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('posts', PostController::class)->only(['create', 'store', 'destroy']);
+    Route::resource('categories', CategoryController::class)->only(['create', 'store', 'destroy']);
+});
+
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::resource('categories', CategoryController::class)->only(['index', 'show']);
 
 require __DIR__.'/auth.php';
